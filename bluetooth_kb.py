c4e3c839c46cc99d41e88267610c127d34bb1ec1
wxv <wxv@users.noreply.github.com>
2015-09-05 00:44:56 -0400

    bluetooth connect

--- bluetooth_kb.py
+++ bluetooth_kb.py
@@ -7,4 +7,27 @@
 #                           | |   | |                            
 #                           |_|   |_|                            
 # Bluetooth to keyboard event
-import bluetooth
+import bluetooth, sys
+
+target_name = "Galaxy S5 CM"
+target_address = None
+
+print("Recommended: pair the phone with Bluetooth.")
+print("Searching for nearby devices...")
+
+nearby_devices = bluetooth.discover_devices(duration=10)
+for bdaddr in nearby_devices:
+    lookup = bluetooth.lookup_name(bdaddr)
+    print(bdaddr, lookup)
+
+    if target_name == lookup:
+        target_address = bdaddr
+        break
+
+if target_address:
+    print("Found target", target_name)
+else:
+    print("Could not find bluetooth device nearby.")
+    sys.exit()
+
+
